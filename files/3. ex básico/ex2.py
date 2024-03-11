c5f8d4aa77af0262767cfe8aa9f256f9712b12ca
Rafael Della Giustina <62893087+RafaelDella@users.noreply.github.com>
2024-03-11 11:07:00 -0300

    Update ex2.py

--- files/3. ex básico/ex2.py
+++ files/3. ex básico/ex2.py
@@ -1,7 +1,7 @@
 """Faça um algoritmo que calcule o salário, com desconto de imposto, de um PJ"""
 valor_hora = float(input("Digite o valor da sua hora: "))
 horas_trabalhadas = float(input("Digite a quantidade das horas trabalhadas: "))
-imposto = float(input("Digite a % do imposto a ser descontado (5% = 5): "))
+imposto = float(input("Digite a % do imposto a ser descontado (5.2% = 5.2): "))
 salario = valor_hora * horas_trabalhadas
 valor_imposto = (valor_hora * horas_trabalhadas) * (imposto/100)
 valor_final = salario - valor_imposto
