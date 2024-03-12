18dd89430a4b49cb984511326668d7625b07d4fd
Rafael Della Giustina <62893087+RafaelDella@users.noreply.github.com>
2024-03-11 22:13:07 -0300

    ex4 finished

---
+++ files/3. ex básico/ex4.py
@@ -0,0 +1,5 @@
+"""Escreva um algoritmo que calcule a área de um triângulo"""
+base = float(input("Digite o valor da base(b) em cm: "))
+altura = float(input("Digite o valor da altura(h) em cm: "))
+area = (base * altura)/2
+print(f'O valor da área do triângulo é de {area:.2f}cm²')
