14f13567dd5b5c539a24c66caf4c7866bc436e30
Rafael Della Giustina <62893087+RafaelDella@users.noreply.github.com>
2024-03-19 10:34:06 -0300

    organization py

--- files/5. Ex Condicionais/ex4.py
+++ files/5. Ex Condicionais/ex4.py
@@ -3,16 +3,6 @@
 considerando o preço normal de etiqueta e a escolha da condição de pagamento. Utilize
 os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o
 cálculo adequado."""
-"""
-Código Condição de pagamento
-• 1 – À vista em dinheiro ou cheque, recebe 10% de desconto
-• 2 – À vista no cartão de crédito, recebe 15% de desconto
-• 3 – Em duas vezes, preço normal de etiqueta sem juros
-• 4 – Em duas vezes, preço normal de etiqueta mais juros de 10%
-"""
-
-"""Crie um algoritmo que calcule o valor da refeição e 
-pergunte ao cliente se ele deseja pagar a taxa de serviço (10%)"""
 
 prato = input('Digite o prato que foi consumido: ')
 quantidade = float(input(f'Digite a quantidade de {prato} consumido: '))
