42cdb7fbda693dda9c4aa5921c9e3476bdcf17d3
Rafael Della Giustina <62893087+RafaelDella@users.noreply.github.com>
2024-03-19 10:32:39 -0300

    Algoritmo de pagamento

--- files/5. Ex Condicionais/ex4.py
+++ files/5. Ex Condicionais/ex4.py
@@ -19,13 +19,21 @@ quantidade = float(input(f'Digite a quantidade de {prato} consumido: '))
 preco = float(input(f'Digite o valor de {prato}: '))
 total = quantidade * preco
 
-"""pagamento = input"""
+print("Forma de pagamento: ")
+print("1 - À vista em dinheiro ou cheque, recebe 10% de desconto ")
+print("2 - À vista no cartão de crédito, recebe 15% de desconto ")
+print("3 - Em duas vezes, preço normal de etiqueta sem juros ")
+print("4 - Em duas vezes, preço normal de etiqueta mais juros de 10% ")
 
-taxa_garcom = input('Você permite incluir a taxa de serviço (10%) ao valor de consumo? Aceito(Sim) ou à Não Aceito(Não)')
-if taxa_garcom == 'Sim':
-    total = total + (total * 0.15)
-
-print(f'O total a ser pago é R${total:.2f}')
-
-
-
+pagamento = int(input("Selecione (1-4) uma forma de pagamento!"))
+if pagamento == 1:
+    total = total - (total * 0.10)
+    print(f"O valor da sua compra é de {total:.2f}")
+elif pagamento == 2:
+    total = total - (total * 0.15)
+    print(f"O valor da sua compra é de {total:.2f}")
+elif pagamento == 3:
+    print(f"O valor da sua compra é de {total:.2f}")
+else:
+    total = total + (total * 0.10)
+    print(f"O valor da sua compra é de {total:.2f}")
