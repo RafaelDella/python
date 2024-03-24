0ae371fb78b2b4a9a987d202e72522f62d1a85ec
Rafael Della Giustina <62893087+RafaelDella@users.noreply.github.com>
2024-03-24 16:28:51 -0300

    refactoring ex 1 and 2

--- files/5. Ex Condicionais/ex2.py
+++ files/5. Ex Condicionais/ex2.py
@@ -3,16 +3,27 @@ para dar uma indicação sobre a condição de peso de uma pessoa adulta. A fór
 = peso / altura2 . Implemente um programa que leia o peso e a altura de um adulto e mostre
 sua condição de acordo com a tabela abaixo."""
 
-peso = float(input("Digite o seu peso em Kg: (75.4kg = 75.4) "))
-altura = float(input("Digite sua altura em cm: (183cm = 183) "))
+import re
+
+def remover_caracteres(string):
+    nova_string = re.sub(r'[^0-9.]', '', string)
+    return nova_string
+
+peso = input("Digite o seu peso em Kg: ")
+peso = float(remover_caracteres(peso))
+altura = input("Digite sua altura em Metros: ")
+altura = float(remover_caracteres(altura))
 imc = peso / (altura * altura)
-print(imc)
 
 if imc < 18.5:
-    print("Você está abaixo do peso")
+    print("Você está abaixo do peso!")
+    print(f"Seu IMC é {imc:2f}")
 elif imc >= 18.5 and imc <= 25:
-    print("Você está no seu peso ideal")
+    print("Você está no seu peso ideal!")
+    print(f"Seu IMC é {imc:2f}")
 elif imc > 25 and imc <= 30:
-    print("Você está com sobre do peso")
+    print("Você está com sobre do peso!")
+    print(f"Seu IMC é {imc:2f}")
 else:
-    print("Você está obeso")
+    print("Você está obeso!")
+    print(f"Seu IMC é {imc:2f}")
